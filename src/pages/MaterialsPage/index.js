d509832cbd9fc5d7f10996aa6ad1425316499adb
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-04-03 16:10:50 -0300

    feat: fetch the subfield from api

--- src/pages/MaterialsPage/index.js
+++ src/pages/MaterialsPage/index.js
@@ -1,7 +1,16 @@
 import { useParams } from "react-router-dom";
+import SkeletonLoading from "../../components/SkeletionLoading/SkeletonLoading";
+import useReadSubfieldById from "../../hooks/api/useReadSubfieldById";
 
 export default function MaterialsPage() {
     const { subfieldId } = useParams();
+    const { subfieldLoading, subfield } = useReadSubfieldById(subfieldId);
 
+    console.log(subfield);
+
+    if (subfieldLoading) {
+        return <SkeletonLoading />;
+    }
+    
     return <div>Hello World {subfieldId}</div>;
 }
